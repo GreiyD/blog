8f7b453029bb7273f618bb69a75d096193cdf992
GreiyD <dima.pidchenko@gmail.com>
2024-07-13 12:00:04 +0300

    feat(routing): add routes for the AuthController and for displaying authentication forms and home page templates.

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::view('/', "home")->name('home');
+Route::view('/about', "about")->name('about');
+
+Route::view('/login-form', "auth.loginForm")->name('login.form');
+Route::view('/register-form', "auth.registerForm")->name('register.form');
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/logout','logout')->name('logout');
+    Route::post('/login', 'login')->name('login');
+    Route::post('/register', 'register')->name('register');
 });
