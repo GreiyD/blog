4fe39882c6f5647a07d9f664d74e819c8de703ce
GreiyD <dima.pidchenko@gmail.com>
2024-07-13 21:57:23 +0300

    feat(routing): add routes for ProfileController

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', "home")->name('home');
@@ -13,4 +14,10 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('/logout','logout')->name('logout');
     Route::post('/login', 'login')->name('login');
     Route::post('/register', 'register')->name('register');
+});
+
+Route::controller(ProfileController::class)->group(function () {
+    Route::get('/profile',  'show')->name('profile.show');
+    Route::get('/profile/edit','showEditForm')->name('profile.edit.form');
+    Route::put('/profile/edit','edit')->name('profile.edit');
 });
