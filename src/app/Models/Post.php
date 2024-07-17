d083adf8aa32ef362e04997a0d1a8540251f7757
GreiyD <dima.pidchenko@gmail.com>
2024-07-17 14:31:03 +0300

    feat: add Post model and migration to create a Posts table in the database
    The enam class(PostStatus) was also added for post statuses

---
+++ src/app/Models/Post.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\PostStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'content',
+        'status',
+        'photo_path',
+    ];
+
+    protected $casts = [
+        'status' => PostStatus::class,
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function reactions()
+    {
+        return $this->hasMany(PostReaction::class);
+    }
+}
