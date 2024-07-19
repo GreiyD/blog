90ed74f77ed572c5443b4e72c11307527bb917a4
GreiyD <dima.pidchenko@gmail.com>
2024-07-20 01:40:00 +0300

    refactor(view): change header and home page templates
    Added the ability to create a new post in the header template to the drop-down list

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -9,4 +9,13 @@
             Guest
         @endif
     </h1>
+{{--    <div class="container mt-5">--}}
+{{--        <div class="row justify-content-center">--}}
+{{--            @foreach($post as $post)--}}
+{{--                <div class="col-md-8">--}}
+{{--                    @include('post.post', ['post' => $post])--}}
+{{--                </div>--}}
+{{--            @endforeach--}}
+{{--        </div>--}}
+{{--    </div>--}}
 @endsection
--- src/resources/views/layouts/header.blade.php
+++ src/resources/views/layouts/header.blade.php
@@ -1,7 +1,7 @@
 <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-            <a class="navbar-brand" href="/">MyApp</a>
+            <a class="navbar-brand" href="/">MyBlog</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -26,8 +26,10 @@
                                 {{ Auth::user()->nickname}}
                             </a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="">Profile</a></li>
+                                <li><a class="dropdown-item" href="{{route('profile.show')}}">Profile</a></li>
                                 <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="{{route('posts.index')}}">My Posts</a></li>
+                                <li><a href="{{route('posts.create')}}" class="dropdown-item">Create post</a></li>
                                 <li>
                                     <a class="dropdown-item" href=""
                                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
