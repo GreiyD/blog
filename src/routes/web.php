5e0525cc4a1e21166c47acefe72a1dcdb7c0ed71
GreiyD <dima.pidchenko@gmail.com>
2024-07-20 01:13:18 +0300

    feat(routing): add routes for working with the PostController, as well as for displaying the post creation form
    - Added user.auth middleware to protect post routes.
    - Grouped post routes under PostController and applied middleware.
    - Added route for post creation form.
    - Routes added:
      - View /post/create (name: posts.create)
      - GET /posts (name: posts.index)
      - GET /posts/{post} (name: posts.show)
      - GET /posts/{post}/edit (name: posts.edit)
      - PUT /posts/{post} (name: posts.update)
      - DELETE /posts/{post} (name: posts.destroy)
      - POST /posts (name: posts.store)

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::view('/', "home")->name('home');
 Route::view('/about', "about")->name('about');
@@ -20,4 +22,17 @@ Route::controller(ProfileController::class)->group(function () {
     Route::get('/profile',  'show')->name('profile.show');
     Route::get('/profile/edit','showEditForm')->name('profile.edit.form');
     Route::put('/profile/edit','edit')->name('profile.edit');
+});
+
+Route::middleware(['user.auth'])->group(function () {
+    Route::view('/post/create', "post.create")->name('posts.create');
+
+    Route::controller(PostController::class, '')->group(function () {
+        Route::get('/posts','index')->name('posts.index');
+        Route::get('/posts/{post}','show')->name('posts.show');
+        Route::get('/posts/{post}/edit','edit')->name('posts.edit');
+        Route::put('/posts/{post}','update')->name('posts.update');
+        Route::delete('/posts/{post}','destroy')->name('posts.destroy');
+        Route::post('/posts','store')->name('posts.store');
+    });
 });
