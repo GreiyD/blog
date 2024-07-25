d2cf61d2e8e4c443a08b2a81452b58faf5430555
GreiyD <dima.pidchenko@gmail.com>
2024-07-26 00:35:53 +0300

    refactor(routing): redo endpoints for profile and authentication controllers

--- src/routes/web.php
+++ src/routes/web.php
@@ -9,19 +9,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/about', "about")->name('about');
 
-Route::view('/login-form', "auth.loginForm")->name('login.form');
-Route::view('/register-form', "auth.registerForm")->name('register.form');
-
 Route::controller(AuthController::class)->group(function () {
+    Route::get('/login','createLogin')->name('login.form');
+    Route::get('/register', 'createRegister')->name('register.form');
+
     Route::post('/logout','logout')->name('logout');
     Route::post('/login', 'login')->name('login');
     Route::post('/register', 'register')->name('register');
 });
 
 Route::controller(ProfileController::class)->group(function () {
-    Route::get('/profile',  'show')->name('profile.show');
-    Route::get('/profile/edit','showEditForm')->name('profile.edit.form');
-    Route::put('/profile/edit','edit')->name('profile.edit');
+    Route::get('/profiles/{profile}',  'show')->name('profile.show');
+    Route::get('/profiles/{profile}/edit','edit')->name('profile.edit');
+    Route::put('/profiles/{profile}','update')->name('profile.update');
 });
 
 Route::middleware(['user.auth'])->group(function () {
@@ -34,6 +34,7 @@ Route::middleware(['user.auth'])->group(function () {
         Route::put('/posts/{post}','update')->name('posts.update');
         Route::delete('/posts/{post}','destroy')->name('posts.destroy');
         Route::post('/posts','store')->name('posts.store');
+
         Route::post('/posts/{post}/reactions','handleReaction')->name('posts.reaction');
     });
 });
