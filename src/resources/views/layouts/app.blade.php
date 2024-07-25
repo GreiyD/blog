5e0107f50319e7b39eb7e8ec8133cf7f52df054b
GreiyD <dima.pidchenko@gmail.com>
2024-07-26 00:34:02 +0300

    feat(view): add padding at the top to the app file and make changes to the header template

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -3,13 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <link rel="stylesheet" href="{{asset('bootstrap/bootstrap.css')}}">
     <title>@yield('title')</title>
 </head>
 <body>
     @include('layouts.header')
 
-    <div class="container">
+    <div class="container mt-5 pt-2">
         @yield('content')
     </div>
 
--- src/resources/views/layouts/header.blade.php
+++ src/resources/views/layouts/header.blade.php
@@ -1,7 +1,7 @@
-<header>
+<header class="fixed-top">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-            <a class="navbar-brand" href="/">MyBlog</a>
+            <a class="navbar-brand" href="{{route('home.index')}}">MyBlog</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -18,24 +18,25 @@
                     </li>
                     @guest
                         <li class="nav-item">
-                            <a class="nav-link {{ request()->is('login-form') ? 'active font-weight-bold' : '' }}" href="{{route('login.form')}}">Login</a>
+                            <a class="nav-link {{ request()->is('login') ? 'active font-weight-bold' : '' }}" href="{{route('login.form')}}">Login</a>
                         </li>
                     @else
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                {{ Auth::user()->nickname}}
+                                {{$user->nickname}}
                             </a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="{{route('profile.show')}}">Profile</a></li>
+                                <li><a class="dropdown-item" href="{{route('profile.show', $user->profile->id)}}">Profile</a></li>
                                 <li><hr class="dropdown-divider"></li>
-                                <li><a class="dropdown-item" href="{{route('posts.index')}}">My Posts</a></li>
+                                <li><a class="dropdown-item" href="">Friends</a></li>
+                                <li><a class="dropdown-item" href="{{route('posts.index')}}">My posts</a></li>
                                 <li><a href="{{route('posts.create')}}" class="dropdown-item">Create post</a></li>
                                 <li>
                                     <a class="dropdown-item" href=""
-                                       onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                       onclick="event.preventDefault(); document.getElementById('logout').submit();">
                                         Logout
                                     </a>
-                                    <form id="logout-form" action="{{route('logout')}}" method="POST" style="display: none;">
+                                    <form id="logout" action="{{route('logout')}}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
                                 </li>
